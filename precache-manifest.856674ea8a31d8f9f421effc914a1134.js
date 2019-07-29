06c23efa92ae24d7be969716f45c15f266c5a94d
hkarambizi <hkarambizi@gmail.com>
2019-07-28 21:57:06 -0400

    Updates

---
+++ precache-manifest.856674ea8a31d8f9f421effc914a1134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a22baa4d5994a62a2a72909eda08f1",
+    "url": "/workbox-react/index.html"
+  },
+  {
+    "revision": "d7f571dedb16483a9967",
+    "url": "/workbox-react/static/css/main.e2af4c97.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/workbox-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d7f571dedb16483a9967",
+    "url": "/workbox-react/static/js/main.6fbb19d5.chunk.js"
+  },
+  {
+    "revision": "0ee2d3cfa3ecb5f44f3a",
+    "url": "/workbox-react/static/js/runtime~main.272b9dc6.js"
+  }
+]);
